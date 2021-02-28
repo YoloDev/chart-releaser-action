e056a1cfffb4a65b5887402f60e5bf00f6ccce64
Aleksander Heintz <alxandr@alxandr.me>
2021-02-28 22:03:24 +0100

    Fix big derp

--- cr.sh
+++ cr.sh
@@ -286,7 +286,7 @@ release_charts() {
 }
 
 update_index() {
-    git remote add charts-index "https://$index_owner:$CR_TOKEN/$index_owner/$index_repo.git"
+    git remote add charts-index "https://$index_owner:$CR_TOKEN@github.com/$index_owner/$index_repo.git"
     git fetch charts-index gh-pages
 
     local args=(-o "$owner" -r "$repo" -c "$charts_repo_url" --remote charts-index --push)
